66e7c81574555cc502bc6c0321a1bbd28894814d
EdWard <ezeward4@gmail.com>
2018-02-10 17:43:28 -0500

    We movin

--- Website/app/index.js
+++ Website/app/index.js
@@ -1,9 +1,19 @@
+var gameArea = null;
+var map = null;
+var deltaX = 0;
+var deltaY = 0;
 
-
+function update()
+{
+	if(deltaX != 0 || deltaY != 0)
+		map.panBy(deltaX, deltaY);
+}
 
 function initMap()
 {
-	var map = new google.maps.Map(document.getElementById('map'), {
+	var overlay = document.getElementById('overlay');
+	
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 42.815102, lng: -73.950355},
 		zoom: 20,
 		disableDefaultUI: true,
@@ -15,4 +25,50 @@ function initMap()
 		rotateControl: true,
 		fullscreenControl: true
 	});
+	
+	gameArea = {
+		canvas : overlay,
+		start : function() {
+			this.context = this.canvas.getContext("2d");
+			this.interval = setInterval(update, 20);
+		},
+		clear : function() {
+			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
+	};
+	
+	gameArea.start();
+	
+	document.addEventListener('keydown', (event) => {
+		switch(event.key)
+		{
+			case 'ArrowUp':
+				deltaY = -10;
+				break;
+			case 'ArrowDown':
+				deltaY = 10;
+				break;
+			case 'ArrowLeft':
+				deltaX = -10;
+				break;
+			case 'ArrowRight':
+				deltaX = 10;
+				break;
+		}
+	});
+	
+	document.addEventListener('keyup', (event) => {
+		switch(event.key)
+		{
+			case 'ArrowUp':
+			case 'ArrowDown':
+				deltaY = 0;
+				break;
+			case 'ArrowLeft':
+			case 'ArrowRight':
+				deltaX = 0;
+				break;
+		}
+	});
+		
 }
