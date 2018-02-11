6d5d010471806c1d2fdc2261521095741fb5f1e6
NicoCobb <cobbn@rpi.edu>
2018-02-11 06:12:35 -0500

    create basic layout for encounter

---
+++ Website/encounter/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mikemang/pen/rjKaGW.
+
+ Template for Vue.js Pokemon Battle Tutorial: 
+
+https://medium.com/@michaelmangial1/vue-js-pokemon-battle-tutorial-380cd72eb681
---
+++ Website/encounter/js/index.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+     userPokemonSrc: "file:///C:/Users/cobbn/Documents/RPI\ All\ Years/HackTechValley/HackTechValley18/truck_boy/battle_boy.gif",
+     opponentPokemonSrc: "file:///C:/Users/cobbn/Documents/RPI\ All\ Years/HackTechValley/HackTechValley18/truck_boy/pixel-art-garbage-truck_1959078.gif",
+     userPokemon: "stickboii",
+     opponentPokemon: "TruCk",
+     userHP: 100,
+     opponentHP: 100,
+     userLevel: 50,
+     opponentLevel: 50,
+     battleText: "What will your boi do?",
+  },
+  
+  methods:{
+    
+  }
+  
+})
