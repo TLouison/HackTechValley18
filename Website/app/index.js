55c583e77cc86966640649d67e70578e553d52fc
Todd <toddmlouison@gmail.com>
2018-02-11 11:11:54 -0500

    delete trucks part 2

--- Website/app/index.js
+++ Website/app/index.js
@@ -44,41 +44,53 @@ function update_locations()
 		}
 		
 	}
-	
 	for(let id in gameArea.markers)
 	{
 		if(trucks === null || !(id in trucks))
 		{
+			var drawTruck = true;
+			for (let dead_id in deadTrucks){
+				if (dead_id == id){ drawTruck=false; }
+
+			}
+			if (drawTruck){
 				marker = gameArea.markers[id];
 				marker.setMap(null);
 				delete gameArea.markers[id];
+			}
 		}
 	}
 
-	for(let id in trucks)
-	{
-		pos = trucks[id];
-		
-		if(id in gameArea.markers)
-		{
-			marker.setPosition(pos);
+	for(let id in trucks){
+		var drawTruck = true;
+		for (let dead_id in deadTrucks){
+			if (dead_id == id){ drawTruck=false; }
 		}
-		else
-		{
-			var url = "../../truck_boy/small-truck.gif"
-			marker = new google.maps.Marker({
-				position: pos,
-				map: map,
-				icon: {
-					url,
-					anchor: new google.maps.Point(50, 50)
-				},
-				title: "garbage"+id
-			});
-			gameArea.markers[id] = marker;
-		}
-		if (checkCollide(pos.lat,pos.lng)) {
-			deadTrucks.push(id);
+
+		if (drawTruck){
+			pos = trucks[id];
+			
+			if(id in gameArea.markers)
+			{
+				marker.setPosition(pos);
+			}
+			else
+			{
+				var url = "../../truck_boy/small-truck.gif"
+				marker = new google.maps.Marker({
+					position: pos,
+					map: map,
+					icon: {
+						url,
+						anchor: new google.maps.Point(50, 50)
+					},
+					title: "garbage"+id
+				});
+				gameArea.markers[id] = marker;
+			}
+			if (checkCollide(pos.lat,pos.lng)) {
+				deadTrucks.add(id);
+			}	
 		}
 	}
 
