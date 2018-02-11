387f9b103468a9642203499a22c74f7e1ed066be
Todd <toddmlouison@gmail.com>
2018-02-11 10:43:45 -0500

    chang filepaths

--- Website/app/js/index.js
+++ Website/app/js/index.js
@@ -1,8 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    userPokemonSrc: "file:///C:/Users/cobbn/Documents/RPI\ All\ Years/HackTechValley/HackTechValley18/truck_boy/battle_boy.gif",
-    opponentPokemonSrc: "file:///C:/Users/cobbn/Documents/RPI\ All\ Years/HackTechValley/HackTechValley18/truck_boy/pixel-art-garbage-truck_1959078.gif",
+    userPokemonSrc: url("../../../truck_boy/battle_boy.gif"),
+    opponentPokemonSrc: url("../../../truck_boy/pixel-art-garbage-truck_1959078.gif"),
     userPokemon: "stickboii",
     opponentPokemon: "TruCk",
     startUserHP: 100,
